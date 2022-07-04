9a53f5e5848ac31cdd5d216629bcb4a4109f9a31
painterhalver <hieptrang13042002@gmail.com>
2022-07-04 16:26:00 +0700

    Register working

--- includes/register.php
+++ includes/register.php
@@ -2,6 +2,27 @@
 session_start();
 
 if (isset($_POST['username']) && isset($_POST['password']) && isset($_POST['confirmPassword'])) {
-    echo "Username: " . $_POST['username'] . "<br>";
-    echo "Password: " . $_POST['password'] . "<br>";
+    // Check if passwords match
+    if ($_POST['password'] !== $_POST['confirmPassword']) {
+        echo "Passwords do not match.";
+        exit();
+    }
+
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $passwordHash = md5($password);
+
+    $db = new SQLite3("../todo.db");
+
+    // Check if username already exists
+    $result = $db->query("SELECT * FROM users WHERE username = '$username'");
+    if ($result->fetchArray()) {
+        echo "Username already exists.";
+        exit();
+    }
+
+    // Add user to database
+    $db->exec("INSERT INTO users (username, password) VALUES ('$username', '$passwordHash')");
+    $_SESSION['user'] = $username;
+    header("Location: /");
 }
--- views/register.php
+++ views/register.php
@@ -8,10 +8,11 @@ require_once(__DIR__ . "/template/header.php");
 ?>
 
 <div class='d-flex justify-content-center align-items-center'>
-    <form action="/includes/register.php" method="POST" style="width: 35%;" oninput="confirmPassword.setCustomValidity(confirmPassword.value !== password.value ? 'Passwords do not match.' : '')">
+    <form method="POST" style="width: 35%;" oninput="confirmPassword.setCustomValidity(confirmPassword.value !== password.value ? 'Passwords do not match.' : '')">
         <div class="form-group mb-2">
             <label for="username" class="mb-1">Username</label>
             <input type="text" class="form-control" id="username" placeholder="Enter username" name="username" required>
+            <div class="invalid-feedback user-exist">Username already exists</div>
         </div>
         <div class="form-group">
             <label for="password" class="mb-1">Password</label>
@@ -22,10 +23,51 @@ require_once(__DIR__ . "/template/header.php");
             <input type="password" class="form-control" id="confirmPassword" placeholder="confirmPassword" name="confirmPassword" required>
         </div>
         <div class="w-100 d-flex justify-content-center">
-            <button type="submit" class="btn btn-primary mt-4">Register</button>
+            <button type="submit" class="btn btn-primary mt-4 submit-btn">Register</button>
         </div>
     </form>
 </div>
+
+<script>
+    const submitBtn = document.querySelector('.submit-btn');
+    const userExist = document.querySelector('.user-exist');
+
+    submitBtn.addEventListener('click', async (e) => {
+        e.preventDefault();
+        const username = document.querySelector('#username').value;
+        const password = document.querySelector('#password').value;
+        const confirmPassword = document.querySelector('#confirmPassword').value;
+
+        if (username.length < 3) {
+            alert('Username must be at least 3 characters');
+            return;
+        }
+        if (password.length < 6) {
+            alert('Password must be at least 6 characters');
+            return;
+        }
+
+        const url = `/includes/register.php`;
+        const formData = new FormData();
+        formData.append('username', username);
+        formData.append('password', password);
+        formData.append('confirmPassword', confirmPassword);
+
+        const res = await fetch(url, {
+            method: "POST",
+            body: formData
+        });
+
+        const data = await res.text();
+        if (data === 'Username already exists.') {
+            userExist.style.display = 'block';
+        } else if (data === 'Passwords do not match.') {
+            alert(data);
+        } else {
+            window.location.href = '/';
+        }
+    });
+</script>
 
 <?php
 require_once(__DIR__ . "/template/footer.php");
